badd4c55978fb948633d92c633bc5837f6ec200e
carlosnsilva <cmanoel17@gmail.com>
2021-05-17 22:29:32 -0300

    Subindo atividade

---
+++ appFilmes/modelo/banco.swift
@@ -0,0 +1,30 @@
+//
+//  banco.swift
+//  appFilmes
+//
+//  Created by IFPB on 16/05/21.
+//  Copyright © 2021 IFPB. All rights reserved.
+//
+
+import Foundation
+
+class Banco {
+    private var Filmes: Array<Filme>
+    
+    init() {
+        self.Filmes = Array<Filme>()
+        <#statements#>
+    }
+    
+    func adicionar(filme: Filme){
+        self.Filmes.append(filme)
+    }
+    
+    func listarFilmes() -> Array<Filmes>{
+        return self.Filmes
+    }
+    
+    func qtdFilmes() -> Int {
+        return self.Filmes.count
+    }
+}
